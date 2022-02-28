8755b19cc6f2bc1f216fd382b36801f8dd37f256
jvolima <jvolima2004@gmail.com>
2022-02-28 17:30:10 -0300

    Finalizando o Header e criação do componente sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import { Flex, Input, Text, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri"
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from "react-icons/ri"
 
 export function Header() {
   return (
     <Flex
       as="header"
       w="100%"
-      maxWidth={1120}
+      maxWidth={1480}
       h="20"
       mx="auto"
       mt="4"
@@ -46,6 +46,35 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20"/>
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20"/>
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>João Vitor Lima</Text>
+            <Text fontSize="small" color="gray.300">
+              jvolima2004@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="João Vitor Lima" src="https://github.com/jvolima.png"/>
+        </Flex>
       </Flex>
     </Flex>
   )
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,7 @@
+import { Flex } from "@chakra-ui/react";
 import Head from "next/head";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
@@ -8,7 +10,13 @@ export default function Dashboard() {
         <title>Dashboard | Dashgo</title>
       </Head>
       
-      <Header />
+      <Flex direction="column" h="100vh">
+        <Header />
+
+        <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+          <Sidebar />
+        </Flex>
+      </Flex>
     </>
   )
 }
