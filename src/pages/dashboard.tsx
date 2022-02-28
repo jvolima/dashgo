140759a17695c6b83380e02957c674b375663edc
jvolima <jvolima2004@gmail.com>
2022-02-28 13:27:18 -0300

    Começando a criação da página Dashboard e também começando o componente Header

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Header } from "../components/Header";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title>Dashboard | Dashgo</title>
+      </Head>
+      
+      <Header />
+    </>
+  )
+}
