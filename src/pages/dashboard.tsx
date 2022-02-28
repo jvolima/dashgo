1b350306f2f7e84b21f745437ffdbb7dba0c0dfe
jvolima <jvolima2004@gmail.com>
2022-02-28 20:41:42 -0300

    Criação dos gráficos na página dashboard com a biblioteca apexcharts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,22 +1,90 @@
-import { Flex } from "@chakra-ui/react";
-import Head from "next/head";
-import { Header } from "../components/Header";
-import { Sidebar } from "../components/Sidebar";
+  import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+  import dynamic from "next/dynamic";
+  import Head from "next/head";
+  import { Header } from "../components/Header";
+  import { Sidebar } from "../components/Sidebar";
 
-export default function Dashboard() {
-  return (
-    <>
-      <Head>
-        <title>Dashboard | Dashgo</title>
-      </Head>
-      
-      <Flex direction="column" h="100vh">
-        <Header />
+  const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false
+  })
 
-        <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-          <Sidebar />
+  const options = {
+    chart: {
+      toolbar: {
+        show: false
+      },
+      zoom: {
+        enabled: false
+      },
+      foreColor: theme.colors.gray[500]
+    },
+    grid: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false
+    },
+    tooltip: {
+      enabled: false
+    },
+    xaxis: {
+      type: "datetime",
+      axisBorder: {
+        color: theme.colors.gray[600]
+      },
+      axisTicks: {
+        color: theme.colors.gray[600]
+      },
+      categories: [
+        '2022-02-22T00:00:00.000Z',
+        '2022-02-23T00:00:00.000Z',
+        '2022-02-24T00:00:00.000Z',
+        '2022-02-25T00:00:00.000Z',
+        '2022-02-26T00:00:00.000Z',
+        '2022-02-27T00:00:00.000Z',
+        '2022-02-28T00:00:00.000Z'
+      ]
+    },
+    fill: {
+      opacity: 0.3,
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        opacityFrom: 0.7,
+        opacityTo: 0.3
+      }
+    }
+  } as const;
+
+  const series = [
+    { name: "series1", data: [31, 120, 10, 28, 51, 18, 109] }
+  ];
+
+  export default function Dashboard() {
+    return (
+      <>
+        <Head>
+          <title>Dashboard | Dashgo</title>
+        </Head>
+        
+        <Flex direction="column" h="100vh">
+          <Header />
+
+          <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+            <Sidebar />
+
+            <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+              <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+                <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+                <Chart options={options} series={series} type="area" height={160}/>
+              </Box>
+              <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+                <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+                <Chart options={options} series={series} type="area" height={160}/>
+              </Box>
+            </SimpleGrid>
+          </Flex>
         </Flex>
-      </Flex>
-    </>
-  )
-}
+      </>
+    )
+  }
