f7da044ade00d324c5c0a9ae0726da4c31a49db3
jvolima <jvolima2004@gmail.com>
2022-03-01 11:09:39 -0300

    Criação do componente Pagination e das páginas de listagem de usuários e criação de novos usuários.

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,130 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import Head from "next/head";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <>
+      <Head>
+        <title>Usuários | Dashgo</title>
+      </Head>
+      <Box>
+        <Header />
+
+        <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+          <Sidebar />
+
+          <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+            <Flex mb="8" justify="space-between" align="center">
+              <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </Flex>
+
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th px="6" color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  <Th>Data de cadastro</Th>
+                  <Th w="8"></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">João Vitor Lima</Text>
+                      <Text fontSize="sm" color="gray.300" >jvolima2004@gmail.com</Text>
+                    </Box>
+                  </Td>
+                  <Td>
+                    01 de Março, 2022
+                  </Td>
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">João Vitor Lima</Text>
+                      <Text fontSize="sm" color="gray.300" >jvolima2004@gmail.com</Text>
+                    </Box>
+                  </Td>
+                  <Td>
+                    01 de Março, 2022
+                  </Td>
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">João Vitor Lima</Text>
+                      <Text fontSize="sm" color="gray.300" >jvolima2004@gmail.com</Text>
+                    </Box>
+                  </Td>
+                  <Td>
+                    01 de Março, 2022
+                  </Td>
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+            <Pagination />
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  )
+}
