e8c4c4c6924d6f22cf80805814e4b281e2d8daff
jvolima <jvolima2004@gmail.com>
2022-03-01 17:27:44 -0300

    Componentização do Pagination e Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>João Vitor Lima</Text>
-        <Text fontSize="small" color="gray.300">
-          jvolima2004@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>João Vitor Lima</Text>
+          <Text fontSize="small" color="gray.300">
+            jvolima2004@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="João Vitor Lima" src="https://github.com/jvolima.png"/>
     </Flex>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "../Header/Logo";
 import { NotificationsNav } from "../Header/NotificationsNav";
 import { Profile } from "../Header/Profile";
 import { SearchBox } from "../Header/SearchBox";
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Flex
       as="header"
@@ -18,11 +23,11 @@ export function Header() {
     >
       <Logo />
 
-      <SearchBox />
+      { isWideVersion && <SearchBox /> }
       
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   )
