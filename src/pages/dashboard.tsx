f9c79546db679beb2fedead8e0432af66be73279
jvolima <jvolima2004@gmail.com>
2022-03-02 12:05:46 -0300

    Responsividade das telas de criação e listagem de usuários

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,90 +1,90 @@
-  import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
-  import dynamic from "next/dynamic";
-  import Head from "next/head";
-  import { Header } from "../components/Header";
-  import { Sidebar } from "../components/Sidebar";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
-  const Chart = dynamic(() => import('react-apexcharts'), {
-    ssr: false
-  })
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
 
-  const options = {
-    chart: {
-      toolbar: {
-        show: false
-      },
-      zoom: {
-        enabled: false
-      },
-      foreColor: theme.colors.gray[500]
+const options = {
+  chart: {
+    toolbar: {
+      show: false
     },
-    grid: {
-      show: false,
-    },
-    dataLabels: {
+    zoom: {
       enabled: false
     },
-    tooltip: {
-      enabled: false
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    enabled: false
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600]
     },
-    xaxis: {
-      type: "datetime",
-      axisBorder: {
-        color: theme.colors.gray[600]
-      },
-      axisTicks: {
-        color: theme.colors.gray[600]
-      },
-      categories: [
-        '2022-02-22T00:00:00.000Z',
-        '2022-02-23T00:00:00.000Z',
-        '2022-02-24T00:00:00.000Z',
-        '2022-02-25T00:00:00.000Z',
-        '2022-02-26T00:00:00.000Z',
-        '2022-02-27T00:00:00.000Z',
-        '2022-02-28T00:00:00.000Z'
-      ]
+    axisTicks: {
+      color: theme.colors.gray[600]
     },
-    fill: {
-      opacity: 0.3,
-      type: 'gradient',
-      gradient: {
-        shade: 'dark',
-        opacityFrom: 0.7,
-        opacityTo: 0.3
-      }
+    categories: [
+      '2022-02-22T00:00:00.000Z',
+      '2022-02-23T00:00:00.000Z',
+      '2022-02-24T00:00:00.000Z',
+      '2022-02-25T00:00:00.000Z',
+      '2022-02-26T00:00:00.000Z',
+      '2022-02-27T00:00:00.000Z',
+      '2022-02-28T00:00:00.000Z'
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3
     }
-  } as const;
-
-  const series = [
-    { name: "series1", data: [31, 120, 10, 28, 51, 18, 109] }
-  ];
-
-  export default function Dashboard() {
-    return (
-      <>
-        <Head>
-          <title>Dashboard | Dashgo</title>
-        </Head>
-        
-        <Flex direction="column" h="100vh">
-          <Header />
-
-          <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-            <Sidebar />
-
-            <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
-              <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
-                <Text fontSize="lg" mb="4">Inscritos da semana</Text>
-                <Chart options={options} series={series} type="area" height={160}/>
-              </Box>
-              <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
-                <Text fontSize="lg" mb="4">Taxa de abertura</Text>
-                <Chart options={options} series={series} type="area" height={160}/>
-              </Box>
-            </SimpleGrid>
-          </Flex>
-        </Flex>
-      </>
-    )
   }
+} as const;
+
+const series = [
+  { name: "series1", data: [31, 120, 10, 28, 51, 18, 109] }
+];
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title>Dashboard | Dashgo</title>
+      </Head>
+        
+      <Flex direction="column" h="100vh">
+        <Header />
+
+        <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+          <Sidebar />
+
+          <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+            <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+              <Chart options={options} series={series} type="area" height={160}/>
+            </Box>
+            <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+              <Chart options={options} series={series} type="area" height={160}/>
+            </Box>
+          </SimpleGrid>
+        </Flex>
+      </Flex>
+    </>
+  )
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
 import Head from "next/head";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
@@ -6,6 +6,11 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <>
       <Head>
@@ -35,17 +40,17 @@ export default function UserList() {
             <Table>
               <Thead>
                 <Tr>
-                  <Th px="6" color="gray.300" width="8">
+                  <Th px={["4", "4", "6"]} color="gray.300" width="8">
                     <Checkbox colorScheme="pink" />
                   </Th>
                   <Th>Usuário</Th>
-                  <Th>Data de cadastro</Th>
+                  { isWideVersion && <Th>Data de cadastro</Th>}
                   <Th w="8"></Th>
                 </Tr>
               </Thead>
               <Tbody>
                 <Tr>
-                  <Td px="6">
+                  <Td px={["4", "4", "6"]}>
                     <Checkbox colorScheme="pink" />
                   </Td>
                   <Td>
@@ -54,10 +59,9 @@ export default function UserList() {
                       <Text fontSize="sm" color="gray.300" >jvolima2004@gmail.com</Text>
                     </Box>
                   </Td>
+                  { isWideVersion && <Td>01 de Março, 2022</Td>}
                   <Td>
-                    01 de Março, 2022
-                  </Td>
-                  <Td>
+                    { isWideVersion && 
                     <Button
                       as="a"
                       size="sm"
@@ -66,11 +70,11 @@ export default function UserList() {
                       leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                     >
                       Editar
-                    </Button>
+                    </Button>}
                   </Td>
                 </Tr>
                 <Tr>
-                  <Td px="6">
+                  <Td px={["4", "4", "6"]}>
                     <Checkbox colorScheme="pink" />
                   </Td>
                   <Td>
@@ -79,10 +83,9 @@ export default function UserList() {
                       <Text fontSize="sm" color="gray.300" >jvolima2004@gmail.com</Text>
                     </Box>
                   </Td>
+                  { isWideVersion && <Td>01 de Março, 2022</Td>}
                   <Td>
-                    01 de Março, 2022
-                  </Td>
-                  <Td>
+                    { isWideVersion && 
                     <Button
                       as="a"
                       size="sm"
@@ -91,11 +94,11 @@ export default function UserList() {
                       leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                     >
                       Editar
-                    </Button>
+                    </Button>}
                   </Td>
                 </Tr>
                 <Tr>
-                  <Td px="6">
+                  <Td px={["4", "4", "6"]}>
                     <Checkbox colorScheme="pink" />
                   </Td>
                   <Td>
@@ -104,10 +107,9 @@ export default function UserList() {
                       <Text fontSize="sm" color="gray.300" >jvolima2004@gmail.com</Text>
                     </Box>
                   </Td>
+                  { isWideVersion && <Td>01 de Março, 2022</Td>}
                   <Td>
-                    01 de Março, 2022
-                  </Td>
-                  <Td>
+                    { isWideVersion && 
                     <Button
                       as="a"
                       size="sm"
@@ -116,7 +118,7 @@ export default function UserList() {
                       leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                     >
                       Editar
-                    </Button>
+                    </Button>}
                   </Td>
                 </Tr>
               </Tbody>
